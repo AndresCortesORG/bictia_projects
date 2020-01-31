bcdff69b9e485967366d016a777933a6036a6fc6
Robinson Andres Cortes Galindo <robinsonandrescortes@gmail.com>
2020-01-31 09:47:05 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function numero (tecla) {
+    var listaNodosPantalla = document.getElementsByClassName('operacion');
+    var nodoTextoPantalla = listaNodosPantalla[0].firstChild;
+    if(tecla=='C'){
+        nodoTextoPantalla.nodeValue = ' ';
+    }else if(tecla=='='){
+        var resultado = eval(nodoTextoPantalla.nodeValue);
+        nodoTextoPantalla.nodeValue = resultado;        
+    }else{
+        nodoTextoPantalla.nodeValue = nodoTextoPantalla.nodeValue + tecla;
+    } 
+}
